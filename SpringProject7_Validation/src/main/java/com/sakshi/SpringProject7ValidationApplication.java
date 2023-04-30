78a7bdbe23dc0b18de44972852a5f21ed83cc8dd
Sakshi0704 <banichoudhary0704@gmail.com>
2023-04-30 16:42:50 +0530

    string_validation

---
+++ SpringProject7_Validation/src/main/java/com/sakshi/SpringProject7ValidationApplication.java
@@ -0,0 +1,13 @@
+package com.sakshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject7ValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject7ValidationApplication.class, args);
+	}
+
+}
